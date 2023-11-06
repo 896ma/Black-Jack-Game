db346879f79ae2ba7b2fe51c5aa38964d2a7f4b9
Barack _ Ouma <127542979+IsoDevMate@users.noreply.github.com>
2023-11-06 15:14:13 +0300

    added to npmjs

--- index.js
+++ index.js
@@ -1,98 +1,129 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+import * as prompts from '@clack/prompts';
+import ora from 'ora';
+import delay from 'delay';
+import { execSync } from 'child_process';
+import readline from 'readline';
 
-//create an array of cards that contains the first card and the second card
-let cards =[]
-let hasblackjack=false
- let isAlive=false
-// 2. Create a variable, sum, and set it to the sum of the two cards
+const dependencies = ['@clack/prompts', 'chalk', 'figlet', 'ora', 'delay'];
 
-// 1. Declare a variable called message and assign its value to an empty string
-let message =""   
-
-// 2. Reassign the message variable to the string we're logging out
-
-let sum =0
-//Button that starts the game whenever clicked
-
-//function  getRandomCard(), that always returns the number  5
-
-function getRandomCard (){
-    let randomNumber =  Math.floor(Math.random() * 13) +1
-    if (randomNumber>10){
-        return 10
-    }else if (randomNumber === 1){
-        return 11
-    }else {
-        return randomNumber
+async function installMissingDependencies() {
+  const missingDependencies = dependencies.filter(dep => {
+    try {
+      require(dep);
+      return false;
+    } catch (e) {
+      return true;
     }
+  });
+
+  if (missingDependencies.length === 0) {
+    console.log('All required dependencies are already installed.');
+    startGame();
+  } else {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(
+      `Some dependencies need to be installed: ${missingDependencies.join(', ')}. Install them now? (Yes/No): `,
+      async (answer) => {
+        rl.close();
+        if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+          await installDependencies(missingDependencies);
+        } else {
+          console.log('Please install dependencies to play.');
+        }
+        startGame();
+      }
+    );
+  }
+}
+
+async function installDependencies(dependenciesToInstall) {
+  console.log('Installing dependencies...');
+  const spinner = ora('Installing dependencies...').start();
+  try {
+    execSync(`npm install ${dependenciesToInstall.join(' ')}`, { stdio: 'ignore' });
+    spinner.succeed(`Installed ${dependenciesToInstall.length} dependencies.`);
+  } catch (e) {
+    spinner.fail('Failed to install dependencies.');
+  }
+}
+
+
+
+
+
+async function startGame() {
+  let cards = [];
+  let hasBlackjack = false;
+  let isAlive = true;
+  let sum = 0;
+
+  console.log('Welcome to the Blackjack game! Try to get as close to 21 as possible without going over.');
+  console.log('You can draw new cards to reach your desired sum.');
+  console.log('Face cards are worth 10, Aces are worth 11 or 1.');
+  console.log('Good luck!\n');
+
+  while (isAlive) {
+    const newCardResponse = await prompts.select({
+      type: 'select',
+      name: 'newCard',
+      message: 'Do you want to draw a new card?',
+      choices: [{ title: 'Yes', value: 'yes' }, { title: 'No', value: 'no' }]
+    });
+
+    if (newCardResponse.newCard === 'yes') {
+      const spinner = ora('Drawing a new card...').start();
+      await delay(2000);
+      const card = getRandomCard();
+      cards.push(card);
+      sum += card;
+      spinner.stop();
+      console.log(`New card: ${card}`);
+
+      if (sum > 21) {
+        console.log('Busted! Your sum is over 21. You lose.');
+        isAlive = false;
+        break;
+      }
+    } else {
+      isAlive = false;
+    }
+  }
+
+  if (hasBlackjack) {
+    console.log('Congratulations! You got Blackjack!');
+  } else {
+    console.log(`Your final sum is ${sum}.`);
+    const restartResponse = await prompts.select({
+      type: 'select',
+      name: 'restart',
+      message: 'Do you want to play again?',
+      choices: [{ title: 'Yes', value: 'yes' }, { title: 'No', value: 'no' }]
+    });
+
+    if (restartResponse.restart === 'yes') {
+      startGame();
+    } else {
+      console.log('Thank you for playing Blackjack!');
+    }
+  }
+}
+
+function getRandomCard() {
+  const randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 
 }
-function startGame(){
-     isAlive=true
-     //Generate two numbers
-     let firstCard=getRandomCard()
-     let secondCard=getRandomCard()
 
-    
-     cards =[firstCard,secondCard]
-     sum= firstCard+secondCard
-     //Re-assign the cards and sum variables so that the game can start
-    renderGame()
-}
-// Write the conditional according to these rules:
+installMissingDependencies();
 
-// if less than or equal to 20 -> "Do you want to draw a new card? "
-let sumEl=document.getElementById("sum-el")
-let messageEl=document.getElementById("message-el")
-let cardsEl=document.getElementById("cards-el")
-let player ={
-    name: "Marvine" ,
-    chips : 150
-}
-let playerEl= document.getElementById("player-el")
-playerEl.textContent =player.name +": $" + player.chips
 
-function renderGame(){
-    cardsEl.textContent= "cards : " 
-
-    //create a for loop to render out all the cards instead of just one
-
-    for (let i=0; i<cards.length;i++){
-        cardsEl.textContent += cards[i] + " "
-    }
-    sumEl.textContent= "sum : " + sum
-    if(sum<=20){
-        message=("Do you want to draw a new card? ")
-    }
-    // else if exactly 21 -> "Wohoo! You've got Blackjack! "s
-    else if(sum===21){
-        message=( " Wonderful! You've got Blackjack:)")
-        hasblackjack=true
-    }
-    // else -> "You're out of the game! "
-    else{
-        message=("You're out of the game! ")
-        isAlive=false
-    }
-    
-
-    //Log it out to the console
-    messageEl.textContent=message
-}
-function newCard(){
-    //only allow the player to get a new card if she IS alive and does NOT  have a blackjack
-
-    if (isAlive==true && hasblackjack===false){
-// 1. Create a card variable, and hard code its value to a number (2-11)
-let card = getRandomCard()
-// 2. Add the new card to the sum variable
-sum += card
-//push the new card to the crads array
-cards.push(card);
-// 3. Call startGame()
-renderGame()
-}
-    }
-    
-    
